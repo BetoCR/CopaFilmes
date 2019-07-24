b753fa0845afc057e2985664ed10b8d4a76bb524
Roberto Cesar Rodrigues <betocr2002@msn.com>
2019-07-24 00:31:57 -0300

    Exclusão de diretório duplicado

--- CopaFilmes/CopaFilmes_Web/copa-filmes-web/src/services/campeonato.js
+++
@@ -1,9 +0,0 @@
-import { http } from './config'
-
-export default {
-    iniciarCampeonato(filmesSelecionados) {
-        return http.post('ChampionShip/Start',{
-            selectedMovies: filmesSelecionados
-        })
-    }
-}
